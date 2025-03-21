24af4f8377f51eaab96508a52d3b4c87d0522590
Juan Diaz <juanjh1>
2025-03-21 16:59:52 -0500

    add states,tie, and validators

--- ttoe/assets_loader.py
+++ ttoe/assets_loader.py
@@ -4,4 +4,6 @@ import pygame
 XPICE_IMG = pygame.image.load("./asset/Xpice.png")
 OPICE_IMG = pygame.image.load("./asset/0pice.png")
 START_IMG = pygame.image.load("./asset/Start.png")
-
+WINNER_IMG = pygame.image.load("./asset/winer.png")
+RESTART_IMG = pygame.image.load("./asset/restart.png")
+TIE_IMG = pygame.image.load("./asset/Tie.png")
--- ttoe/run.py
+++ ttoe/run.py
@@ -2,9 +2,19 @@ import pygame
 from utils import center_rects, draw_lines, Status
 from Pices import Pice, XPice, OPice
 from Table import Table
-from assets_loader import START_IMG
+from assets_loader import (START_IMG, 
+                           WINNER_IMG, 
+                           XPICE_IMG,
+                            OPICE_IMG,
+                            TIE_IMG, RESTART_IMG)
 # pygame setup
 pygame.init()
+
+turn_dict = {
+    "x" : (XPice, "o"),
+    "o": (OPice, "x"),
+
+}
 
 
 
@@ -25,9 +35,11 @@ main_rect = pygame.Rect(center_rects(WIDTH, MAIN_RECT_WIDTH) , center_rects(HEIG
 table = None
 
 turn = "x"
-
+winer_img = None
 current_status = Status.MENU
-
+next_turn = 1
+pice_in_game = 0
+tie_conunter = 0
 
 while running:
     for event in pygame.event.get():
@@ -43,36 +55,52 @@ while running:
                     x = (pos[cartesian_x] - main_rect.left) // PIXEL_X
                     y = (pos[cartesian_y] - main_rect.top) // PIXEL_Y
                     if(table.validate_pice_oposite(x, y)):
-                        if(turn == "x"):
-                            table.put_pice(x, y, XPice) 
-                            turn = "o"
-                        else:
-                            table.put_pice(x, y, OPice) 
-                            turn = "x"
-                    
+                            current_pice = turn_dict[turn]
+                            table.put_pice(x, y, current_pice[pice_in_game]) 
+                            turn = current_pice[next_turn]
+                            tie_conunter += 1; 
+                       
                     table.print_table()
-
+                   
+                    print(tie_conunter)
                     if table.validate_winner(XPice):
-                        print("gano x")
-                        current_status = Status.MENU
+                        winer_img = XPICE_IMG
+                        current_status = Status.WINNER
                     if table.validate_winner(OPice):
-                        print("gano o")
-                        current_status = Status.MENU
+                        winer_img = OPICE_IMG
+                        current_status = Status.WINNER
+                    if tie_conunter == 9:
+                         current_status = Status.WINNER
+
             if current_status == Status.MENU:
                 print(center_rects(WIDTH, 300) )
                 if center_rects(WIDTH, 300) < pos[cartesian_x] and (center_rects(WIDTH, 300) < pos[cartesian_x] and pos[cartesian_y] > 90 and pos[cartesian_y] < 150 + 90 ):
                     current_status = Status.INGAME
+            if current_status == Status.WINNER:
+                print(center_rects(WIDTH, 300) )
+                if center_rects(WIDTH, 300) < pos[cartesian_x] and (center_rects(WIDTH, 300) < pos[cartesian_x] and pos[cartesian_y] > 90 and pos[cartesian_y] < 150 + 90 ):
+                    current_status = Status.MENU
 
 
     screen.fill("purple")
     if(current_status == Status.MENU):
+        tie_conunter = 0
         screen.blit(START_IMG, (center_rects(WIDTH, 300), 90))
-
         table = Table()
 
     if(current_status == Status.INGAME):
         screen.fill("blue", main_rect )
         draw_lines(main_rect,Table.SIZE, screen, table)
+
+    if(current_status == Status.WINNER):
+        screen.blit(RESTART_IMG, (center_rects(WIDTH, 300), 90))
+        if(winer_img != None):
+            screen.blit(WINNER_IMG, (center_rects(WIDTH, 300), 400))
+            
+            screen.blit(winer_img, (center_rects(WIDTH, 120), 220))
+        else:
+            screen.blit(TIE_IMG, (center_rects(WIDTH, 300), 400))
+       
 
     pygame.display.flip()
 
